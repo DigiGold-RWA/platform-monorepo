6bb340af5e92be0bcf508b523b4ceb822b2b16a0
PEeGEe21 <mailpraiseudeh@gmail.com>
2023-11-23 18:20:51 +0100

    updated report page

--- platform/app/components/IconComponent.jsx
+++ platform/app/components/IconComponent.jsx
@@ -631,7 +631,56 @@ const CopyIcon = () => {
   );
 };
 
+const CalendarIcon = ({size}) => {
+  return (
+    <>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width={`${size ? size : '32'}`}
+        height={`${size ? size : '32'}`}
+        viewBox="0 0 24 24"
+        fill="none"
+      >
+        <path
+          d="M8 2v3M16 2v3"
+          stroke="#FFCC29"
+          strokeWidth="1.5"
+          strokeMiterlimit="10"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        ></path>
+        <path
+          opacity=".4"
+          d="M3.5 9.09h17"
+          stroke="#FFCC29"
+          strokeWidth="1.5"
+          strokeMiterlimit="10"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        ></path>
+        <path
+          d="M21 8.5V17c0 3-1.5 5-5 5H8c-3.5 0-5-2-5-5V8.5c0-3 1.5-5 5-5h8c3.5 0 5 2 5 5Z"
+          stroke="#FFFFFF"
+          strokeWidth="1.5"
+          strokeMiterlimit="10"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        ></path>
+        <path
+          opacity="1"
+          d="M11.995 13.7h.009M8.295 13.7h.01M8.295 16.7h.009"
+          stroke="#FFCC29"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        ></path>
+      </svg>
+    </>
+  );
+};
+
 export {
+  CalendarIcon,
   MapIcon,
   EyeIcon,
   CopyIcon,
--- platform/app/report/page.js
+++ platform/app/report/page.js
@@ -4,6 +4,8 @@ import Footer from '../components/HomePageLayout/Footer';
 import Link from 'next/link';
 import PricingTable from '../components/PricingTable';
 import Image from 'next/image';
+import { Calendar2 } from 'iconsax-react';
+import { CalendarIcon } from '../components/IconComponent';
 
 const Report = () => {
   return (
@@ -30,20 +32,127 @@ const Report = () => {
               </Link>
             </div>
           </div>
-
-          
         </div>
-        {/* <Image
+        <div className='h-42 w-44 '>
+          <Image
             src="/images/bq(3).svg"
             alt="bg"
-            width={600}
-            height={250}
-            // fill 
-            className="object-center object-contain absolute left-[4  00px] pointer-events-none max-w-3xl mx-auto "
-          /> */}
+            // width={600}
+            // height={250}
+            fill 
+            className="object-center object-contain  pointer-events-none max-w-3xl mx-auto mt-20 "
+          />
+        </div>
+        
       </section>
 
-      <section className="pt-[80px] lg:pt-[120px] pb-[80px] lg:pb-[120px]">
+      <section className=" pt-[80px] lg:pt-[100px] pb-[60px] lg:pb-[80px]">
+        <div className="container max-w-7xl mx-auto px-4 lg:px-0 ">
+          <div className="relative pt-5 pb-10">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4 lg:gap-8">
+              {/* {CalendarData.map((cal, index) => ( */}
+                {/* <> */}
+                  <div className="card gap-[2rem] p-[2rem] rounded-2xl  bg-card-background text-white">
+                    <div className="flex items-start justify-start gap-3">
+                      <div className="mt-1">
+                        <CalendarIcon variant="TwoTone" size={22} />
+                      </div>
+                      <div className="space-y-2 flex-1">
+                        <h2 className="card-title text-xl font-semibold">2023</h2>
+                        <div className='flex items-center justify-between flex-wrap text-base text-[#FFCC29]'>
+                          <p className="">Jan</p>
+                          <p className="">Feb</p>
+                          <p className="">Mar</p>
+                          <p className="">Apr</p>
+                          <p className="">May</p>
+                          <p className="">Jun</p>
+                          <p className="">Aug</p>
+                          <p className="">Sep</p>
+                          <p className="">Oct</p>
+                          <p className="">Nov</p>
+                          <p className="">Dec</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="card gap-[2rem] p-[2rem] rounded-2xl  bg-card-background text-white">
+                    <div className="flex items-start justify-start gap-3">
+                      <div className="mt-1">
+                        <CalendarIcon variant="TwoTone" size={22} />
+                      </div>
+                      <div className="space-y-2 flex-1">
+                        <h2 className="card-title text-xl font-semibold">2022</h2>
+                        <div className='flex items-center justify-between flex-wrap text-base text-[#FFCC29]'>
+                          <p className="">Jan</p>
+                          <p className="">Feb</p>
+                          <p className="">Mar</p>
+                          <p className="">Apr</p>
+                          <p className="">May</p>
+                          <p className="">Jun</p>
+                          <p className="">Aug</p>
+                          <p className="">Sep</p>
+                          <p className="">Oct</p>
+                          <p className="">Nov</p>
+                          <p className="">Dec</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="card gap-[2rem] p-[2rem] rounded-2xl  bg-card-background text-white">
+                    <div className="flex items-start justify-start gap-3">
+                      <div className="mt-1">
+                        <CalendarIcon variant="TwoTone" size={22} />
+                      </div>
+                      <div className="space-y-2 flex-1">
+                        <h2 className="card-title text-xl font-semibold">2021</h2>
+                        <div className='flex items-center justify-between flex-wrap text-base text-[#FFCC29]'>
+                          <p className="">Jan</p>
+                          <p className="">Feb</p>
+                          <p className="">Mar</p>
+                          <p className="">Apr</p>
+                          <p className="">May</p>
+                          <p className="">Jun</p>
+                          <p className="">Aug</p>
+                          <p className="">Sep</p>
+                          <p className="">Oct</p>
+                          <p className="">Nov</p>
+                          <p className="">Dec</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="card gap-[2rem] p-[2rem] rounded-2xl  bg-card-background text-white">
+                    <div className="flex items-start justify-start gap-3">
+                      <div className="mt-1">
+                        <CalendarIcon size={22}/>
+                      </div>
+                      <div className="space-y-2 flex-1">
+                        <h2 className="card-title text-xl font-semibold">2020</h2>
+                        <div className='flex items-center justify-between flex-wrap text-base text-[#FFCC29]'>
+                          <p className="">Jan</p>
+                          <p className="">Feb</p>
+                          <p className="">Mar</p>
+                          <p className="">Apr</p>
+                          <p className="">May</p>
+                          <p className="">Jun</p>
+                          <p className="">Aug</p>
+                          <p className="">Sep</p>
+                          <p className="">Oct</p>
+                          <p className="">Nov</p>
+                          <p className="">Dec</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div> 
+                {/* </> */}
+               {/* ))}  */}
+              
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className=" pt-[80px] lg:pt-[100px] pb-[80px] lg:pb-[120px]">
         <div className="container mx-auto max-w-6xl ">
           <div className="flex flex-col justify-center items-center w-full max-w-4xl mx-auto mb-3 pb-8">
             <h1 className="text-center text-4xl font-bold leading-snug text-white">
